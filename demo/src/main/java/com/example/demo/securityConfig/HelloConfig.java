bb19ef1c2cc2c9aa7474f189aa952ae547871661
Yogeth <yogethcr@gmail.com>
2026-01-05 12:20:51 +0530

    update HelloConfig.java

--- demo/src/main/java/com/example/demo/securityConfig/HelloConfig.java
+++ demo/src/main/java/com/example/demo/securityConfig/HelloConfig.java
@@ -2,12 +2,16 @@ package com.example.demo.securityConfig;
 import org.springframework.context.annotation.*;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 public class HelloConfig{
   @Bean 
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
   {
+    http.csrf(customizer->customizer.disable());
+    http.authorizeHttpRequest(request->request.anyRequest().authendicated());
+    
     return http.build();
     
   }
