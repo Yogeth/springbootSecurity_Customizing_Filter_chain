8f458ee6fbd7fa46480d71ffbc6e4a43af62aabe
Yogeth <yogethcr@gmail.com>
2026-01-05 11:20:56 +0530

    update

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
+@RestController
+public class HelloController{
+  
+  @GetMapping("/welcome")
+  public String greeting(){
+    return HELLO IAM YOGETH;
+  }
+}
