276cdeaf41899e99c5e8e9d77e6d64a3d9a955c4
Yogeth <yogethcr@gmail.com>
2026-01-06 15:14:42 +0530

    update HelloConfig.java

--- demo/src/main/java/com/example/demo/securityConfig/HelloConfig.java
+++ demo/src/main/java/com/example/demo/securityConfig/HelloConfig.java
@@ -18,7 +18,7 @@ public class HelloConfig{
     http.authorizeHttpRequests(request->request.anyRequest().authenticated());
     http.formLogin(Customizer.withDefaults());
     http.httpBasic(Customizer.withDefaults());
-    http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+  //  http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
     
   }
