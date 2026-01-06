8dd2ffd95b9c09f5f4dc9edd3bc35af96ab39c97
Yogeth <yogethcr@gmail.com>
2026-01-06 15:11:15 +0530

    update HelloConfig.java

--- demo/src/main/java/com/example/demo/securityConfig/HelloConfig.java
+++ demo/src/main/java/com/example/demo/securityConfig/HelloConfig.java
@@ -4,6 +4,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class HelloConfig{
@@ -12,10 +15,25 @@ public class HelloConfig{
   {
     http.csrf(customizer->customizer.disable());
     http.authorizeHttpRequests(request->request.anyRequest().authenticated());
-//    http.formLogin(Customizer.withDefaults());
+    http.formLogin(Customizer.withDefaults());
     http.httpBasic(Customizer.withDefaults());
     http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
     
+  }
+  
+  @Bean 
+  public UserDetailsService userDetailsService(){
+    User user1=User.username("Wayne")
+                   .password(9874)
+                   .roles("ADMIN")
+                   .build();
+     
+     User user2=User.username("Bruce")
+                   .password(9884)
+                   .roles("USER")
+                   .build();              
+                   
+    return InMemoryUserDetailsManager(user1,user2);
   }
 }
